29c3668fc0a60b05528c84d2047ece55ce85e516
NaraLuwan <hqcadmin@163.com>
2019-10-29 21:27:59 +0800

    add AesUtils

---
+++ RsaAndAes/AesUtils.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RsaAndAes
+{
+    public class Utils
+    {
+        public static string ByteArrayToHexString(sbyte[] ba)
+        {
+            // return BitConverter.ToString(ba).Replace("-", "");
+            return "";
+        }
+    }
+
+    public class AesUtils
+    {
+        static byte[] IV = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+        /**
+         * 加密字符串
+         */
+        public static string EncryptString(string plainText, byte[] key)
+        {
+            byte[] enc = EncryptStringToBytes(plainText, key);
+            return Convert.ToBase64String(enc);
+        }
+
+        /**
+         * 解密字符串
+         */
+        public static string DecryptString(string cipherText, string key)
+        {
+            return DecryptStringFromBytes(Convert.FromBase64String(cipherText), key);
+        }
+
+        public static byte[] EncryptStringToBytes(string plainText, byte[] Key)
+        {
+            // Check arguments. 
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            byte[] encrypted;
+            // Create an RijndaelManaged object 
+            // with the specified key and IV. 
+            RijndaelManaged rijAlg = new RijndaelManaged
+            {
+                Key = Key,
+                Mode = System.Security.Cryptography.CipherMode.CBC,
+                Padding = System.Security.Cryptography.PaddingMode.PKCS7,
+                IV = IV
+            };
+
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+            // Create the streams used for encryption. 
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+
+                        //Write all data to the stream.
+                        swEncrypt.Write(plainText);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+
+
+
+            // Return the encrypted bytes from the memory stream. 
+            return encrypted;
+
+        }
+
+        public static byte[] EncryptStringToBytes(string plainText, string Key)
+        {
+            // Check arguments. 
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            byte[] encrypted;
+            // Create an RijndaelManaged object 
+            // with the specified key and IV. 
+            RijndaelManaged rijAlg = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(Key),
+                Mode = System.Security.Cryptography.CipherMode.CBC,
+                Padding = System.Security.Cryptography.PaddingMode.PKCS7,
+                IV = IV
+            };
+
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+            // Create the streams used for encryption. 
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+
+                        //Write all data to the stream.
+                        swEncrypt.Write(plainText);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+
+
+
+            // Return the encrypted bytes from the memory stream. 
+            return encrypted;
+
+        }
+
+        public static string DecryptStringFromBytes(byte[] cipherText, string Key)
+        {
+            // Check arguments. 
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            // Declare the string used to hold 
+            // the decrypted text. 
+            string plaintext = null;
+
+            // Create an RijndaelManaged object 
+            // with the specified key and IV. 
+            RijndaelManaged rijAlg = new RijndaelManaged()
+            {
+                Key = Encoding.UTF8.GetBytes(Key),
+                Mode = System.Security.Cryptography.CipherMode.CBC,
+                Padding = System.Security.Cryptography.PaddingMode.PKCS7
+            };
+
+            rijAlg.IV = IV;
+
+            // Create a decrytor to perform the stream transform.
+            ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+            // Create the streams used for decryption. 
+            using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+
+                        // Read the decrypted bytes from the decrypting stream 
+                        // and place them in a string.
+                        plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+
+
+
+            return plaintext;
+
+        }
+    }
+}
--- RsaAndAes/Program.cs
+++ RsaAndAes/Program.cs
@@ -6,7 +6,29 @@ namespace RsaAndAes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            testAes();
+
+            Console.Read();
+        }
+
+        public static void testAes()
+        {
+            try
+            {
+                string originalStr = "Hello AES!";
+                string aesKey = "testKey!testKey!";
+                Console.WriteLine("originalStr:   {0}", originalStr);
+
+                byte[] encrypted = AesUtils.EncryptStringToBytes(originalStr, aesKey);
+                string decryptStr = AesUtils.DecryptStringFromBytes(encrypted, aesKey);
+
+                Console.WriteLine("decryptStr: {0}", decryptStr);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
         }
     }
 }
